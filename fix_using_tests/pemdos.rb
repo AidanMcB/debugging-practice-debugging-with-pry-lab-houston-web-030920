5a1f078cdfb3daac2a65aaf6c9a609299e0e27ea
Aidan K McBride <aidankmcbride@gmail.com>
2020-02-25 21:08:57 +0000

    Automatically backed up by Learn

--- fix_using_tests/pemdos.rb
+++ fix_using_tests/pemdos.rb
@@ -1,9 +1,14 @@
 require 'pry'
 def snake_it_up(string)
-  if string[0] == "s"
-   "ssssssssss" += string 
-  return string
-  else
-  string
-  end
+   if string[0] == "s"
+  10 * "s" + string	   
+  i = 0
+    while i < 10 do
+      string.prepend("s")
+      i += 1
+    end
+    string
+  else	  else
+  string	    string
+  end	  end
 end
